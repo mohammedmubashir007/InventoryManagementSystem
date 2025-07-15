dbe877c3c6be1710d0aa4797d0a34424332ec51e
Mohammed Mubashir <mohammedmubashir204@gmail.com>
2025-07-15 09:58:46 +0530

    Created Auth Service Models and DTOs

---
+++ Services/AuthService/Models/User.cs
@@ -0,0 +1,10 @@
+namespace Services.AuthService.Models;
+
+public class User
+{
+    public string Id { get; set; }
+    public string Username { get; set; }
+    public string Email { get; set; }
+    public string PasswordHash { get; set; }
+
+}
